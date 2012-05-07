44e36ed354bc0506f31293531c24036f0af32ea4
Daniel Robbins <drobbins@funtoo.org>
2012-05-07 16:45:10 +0000

    GPLv2 licensing fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 NAME = "ZenPacks.community.SuSE"
 VERSION = "1.1"
 AUTHOR = "Eric Edgar"
-LICENSE = ""
+LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
 PACKAGES = ['ZenPacks', 'ZenPacks.community', 'ZenPacks.community.SuSE']
 INSTALL_REQUIRES = ['ZenPacks.community.LinuxMonitorAddOn>=1.0']
